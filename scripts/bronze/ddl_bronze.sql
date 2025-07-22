5d9b49e50d535fd510b952922a83473cd904d9b6
Helaczek03 <otajchert1@wp.pl>
2025-07-22 17:52:31 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -14,7 +14,7 @@ go
 create table bronze.crm_cust_info (
 	cst_id int,
 	cst_key nvarchar(50),
-	cst_fistname nvarchar(50),
+	cst_firstname nvarchar(50),
 	cst_lastname nvarchar(50),
 	cst_material_status nvarchar(50),
 	cst_gndr nvarchar(50),
